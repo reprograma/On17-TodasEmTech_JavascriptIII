23450f5e24416e8f768fbdb1e3cad0bf2e756a9e
Ayane Martins <ayanecm@hotmail.com>
2022-06-05 00:12:59 -0300

    Desafio

---
+++ ayane-martins/script.js
@@ -0,0 +1,83 @@
+/* 
+
+O exercício consiste em usar JavaScript para popular os campos corretamente com os dados do arquivo data.json (ou do objeto JSON) um site de informações sobre séries protagonizadas por mulheres trans e travestis, neste caso a página exibe 5 séries, em 5 cards diferentes em uma só página;
+
+Importante:
+
+1. Crie seu próprio layout usando HTML e CSS nos arquivos já criados nesta pasta;
+2. Repare que este Json é uma matriz, logo é possível usar métodos de array para acessar seu conteúdo;
+3. Entregue este exercício da maneira que conseguir, use o exemplo da sala para guiar nesta construção;
+
+*/
+
+let objetoJson = [
+  {
+    "imagem": "./images/pose.jpg",
+    "titulo": "Pose",
+    "ano": "2018",
+    "diretor": "Ryan Murphy",
+    "generos": ["Drama"],
+    "elenco": ["Dominique Jackson", "Indya Moore", "Mj Rodriguez", "Angelica Ross", "Hailie Sahar"], 
+    "instagram": "https://www.instagram.com/poseonfx/"
+  },
+  {
+    "imagem": "./images/manhas.jpg",
+    "titulo": "Manhãs de Setembro",
+    "ano": "2021",
+    "diretor": "Luis Pinheiro",
+    "generos": ["Drama"],
+    "elenco": ["Liniker", "Linn da Quebrada"], 
+    "instagram": "https://www.instagram.com/explore/tags/manhasdesetembroserie/"
+  },
+  {
+    "imagem": "./images/euphoria.jpg",
+    "titulo": "Euphoria",
+    "ano": "2019",
+    "diretor": "Sam Levinson",
+    "generos": ["Drama"],
+    "elenco": ["Hunter Schafer", "Ron Leshem", "Daphna Levin"], 
+    "instagram": "https://www.instagram.com/euphoria/"
+  },
+  {
+    "imagem": "./images/veneno.jpeg",
+    "titulo": "Veneno",
+    "ano": "2020",
+    "diretor": "Javier Ambrossi",
+    "generos": ["Drama", "Biografia"],
+    "elenco": ["Daniela Santiago", "Jedet Sánchez", "Isabel Torres", "Lola Rodríguez", "Paca La Piraña"], 
+    "instagram": "https://www.instagram.com/venenolaserie/"
+  },
+  {
+    "imagem": "./images/legendary.jpg",
+    "titulo": "Legendary",
+    "ano": "2020",
+    "diretor": "Rik Reinholdtsen",
+    "generos": ["Reality Show", "Competição"],
+    "elenco": ["Leiomy Maldonado", "Megan Thee Stallion", "Dashaun Wesley"], 
+    "instagram": "https://www.instagram.com/legendarymax/"
+  },
+]
+
+const vincularDados = () => {
+  objetoJson.forEach((elemento, index) => {
+const foto = document.getElementById(`photography-${index}`)
+const titulo = document.getElementById(`title-${index}`)
+const ano = document.getElementById(`year-${index}`)
+const generos = document.getElementById(`gender-${index}`)
+const elenco = document.getElementById(`cast-${index}`)
+const instagram = document.getElementById(`instagram-${index}`)
+
+foto.setAttribute('src', elemento.imagem)
+titulo.innerText = elemento.titulo
+ano.innerHTML = elemento.ano
+generos.innerHTML = elemento.generos
+elenco.innerText = elemento.elenco.join(' - ')
+instagram.setAttribute('href', elemento.instagram)
+  });
+}
+
+vincularDados()
+
+
+
+
