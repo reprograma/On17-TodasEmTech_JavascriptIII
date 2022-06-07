dfa35de9d96d5bcc0b01cd0514cc7c8eacfbf437
Juliana <juliana@MacBook-Pro-de-Juliana.local>
2022-06-06 21:43:32 -0300

    Entrega do desafio

--- revisao/script.js
+++ revisao/script.js
@@ -1,1 +1,18 @@
+const horario = 10
 
+if(horario >= 0 && horario < 6) {
+    console.log("madrugada")
+} else if (horario >= 6 && horario <12) {
+    console.log("manhã")
+} else if (horario >= 12 && horario <18) {
+    console.log("tarde");
+} else if (horario >= 18 && horario <24){
+    console.log("noite");
+} else {
+    console.log("horario invalido");
+}
+
+switch (horario) {
+  case 0: 
+  
+}
