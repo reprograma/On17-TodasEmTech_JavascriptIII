22039f32bc8eb36102664b5c41e555e7a60dfdba
Emanueli Martins <emanueli.martiins@gmail.com>
2022-06-12 11:51:52 -0300

    resolucao desafio

--- desafio/script.js
+++ desafio/script.js
@@ -9,3 +9,22 @@ Importante:
 3. Entregue este exercício da maneira que conseguir, use o exemplo da sala para guiar nesta construção;
 
 */
+const imagem = document.getElementById('imagem')
+const titulo = document.getElementById('title')
+const ano = document.getElementById('ano')
+const diretor = document.getElementById('diretor')
+const generos = document.getElementById('gender')
+const elenco = document.getElementById('elenco')
+const instagram = document.getElementById('instagram')
+
+const preencherDados = () => {
+  imagem.setAttribute('src', objetoJson.imagem)
+  titulo.innerText = objetoJson.nome
+  ano.innerHTML  = objetoJson.ano.join(' - ')
+  diretor.innerHTML = objetoJson.diretor.join(' - ')
+  generos.innerHTML = objetoJson.generos.join(' - ')
+  elenco.innerHTML = objetoJson.elenco.join(' - ')
+  instagram.setAttribute('href', objetoJson.instagram)
+}
+
+preencherDados()
