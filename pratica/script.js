5b3ccf80638f1179fa9515792e61434a1000bb31
luinemauricio <luinemauricio@hotmail.com>
2022-06-28 01:19:27 -0300

    entregando o exercicio

--- pratica/script.js
+++ pratica/script.js
@@ -6,7 +6,9 @@ const objetoJson = {
   instagram: "https://www.instagram.com/linikeroficial/",
 };
 
-// Suponha que nosso projeto consiste em um site de informações sobre cantoras brasileiras e na página em questão a usuária selecionou a cantora Liniker, e a API nos retornou um JSON com todas as informações que precisamos (armazenado na variável objetoJson acima), então devemos popular nosso HTML com essas informações utilizando JavaScript
+// Suponha que nosso projeto consiste em um site de informações sobre cantoras brasileiras e na página em questão a usuária selecionou a cantora Liniker, 
+//e a API nos retornou um JSON com todas as informações que precisamos (armazenado na variável objetoJson acima), então devemos popular nosso HTML com essas informações
+//utilizando JavaScript
 
 const foto = document.getElementById('photography')
 const titulo = document.getElementById('title')
